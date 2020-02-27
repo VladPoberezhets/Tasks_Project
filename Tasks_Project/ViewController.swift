26c7cbd4455fc6b5de35cdc9adae0022fe85cc06
Vlad <vladpoberezhets123@gmail.com>
2020-02-27 21:06:50 +0200

    Initial Commit

---
+++ Tasks_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks_Project
+//
+//  Created by Vlad on 27.02.2020.
+//  Copyright © 2020 Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
